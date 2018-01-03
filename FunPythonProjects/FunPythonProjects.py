6af3f2a2308112ced48549cc6de0a6ee61929031
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-03 08:14:40 -0500

    Added some copy, fixed list assignment

--- FunPythonProjects/FunPythonProjects.py
+++ FunPythonProjects/FunPythonProjects.py
@@ -2,9 +2,21 @@ import random
 #Dice Roller
 def dice_roller(num_dice, num_sides, num_bonus):
     result = 0
-    rolls = int[num_dice]
+    rolls = [num_dice]
     for roll in rolls:
         roll = random.randint(1, num_sides)
         result +=roll
     result += num_bonus
     print(result)
+
+def main ():
+    go_again = True
+    while go_again:
+        print("Let's roll some dice! (or type \"quit\" to, well, you know).")
+        num_dice = int(input("How many dice are you rolling?\n"))
+        num_sides = int(input("How many sides do the dice have?\n"))
+        num_bonus = int(input("Any modifiers?\n"))
+        print("Voila!")
+        dice_roller(num_dice,num_sides,num_bonus)
+
+main()
