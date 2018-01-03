79d1ed7242cb6be8a64f7405c7d75446ecd8771d
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-02 21:11:15 -0500

    Added dice_roller

--- FunPythonProjects/FunPythonProjects.py
+++ FunPythonProjects/FunPythonProjects.py
@@ -1,14 +1,14 @@
-from _random import Random
+import random
 #Dice Roller
 def dice_roller(num_dice, num_sides, num_bonus):
     result = 0
-    roll = Random.random(1, num_sides + 1)
+    roll = random.randint(1, num_sides)
     print(roll)
 
 num_sides = None
 while num_sides != "quit":
     num_sides = input("How many sides on the die?")
-    print(dice_roller(1,int(num_sides),0))
+    dice_roller(1,int(num_sides),0)
     num_sides = input("Enter a number to roll again, or \"quit\" to stop.")
 
 
