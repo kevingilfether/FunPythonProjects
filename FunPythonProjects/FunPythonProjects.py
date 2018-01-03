e741dc7c0aa0988fc2f98d8ea1d5489919198284
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-02 20:45:46 -0500

    Trying to get random to work

--- FunPythonProjects/FunPythonProjects.py
+++ FunPythonProjects/FunPythonProjects.py
@@ -1,4 +1,15 @@
+from _random import Random
 #Dice Roller
+def dice_roller(num_dice, num_sides, num_bonus):
+    result = 0
+    roll = Random.random(1, num_sides + 1)
+    print(roll)
 
-def dice_roller (num_dice, num_sides, num_bonus):
+num_sides = None
+while num_sides != "quit":
+    num_sides = input("How many sides on the die?")
+    print(dice_roller(1,int(num_sides),0))
+    num_sides = input("Enter a number to roll again, or \"quit\" to stop.")
 
+
+    
