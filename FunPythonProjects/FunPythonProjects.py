9e10961d4c9864a226944c241c547f4fbc1829d6
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-02 21:17:49 -0500

    Finished DiceRoller

--- FunPythonProjects/FunPythonProjects.py
+++ FunPythonProjects/FunPythonProjects.py
@@ -2,14 +2,9 @@ import random
 #Dice Roller
 def dice_roller(num_dice, num_sides, num_bonus):
     result = 0
-    roll = random.randint(1, num_sides)
-    print(roll)
-
-num_sides = None
-while num_sides != "quit":
-    num_sides = input("How many sides on the die?")
-    dice_roller(1,int(num_sides),0)
-    num_sides = input("Enter a number to roll again, or \"quit\" to stop.")
-
-
-    
+    rolls = int[num_dice]
+    for roll in rolls:
+        roll = random.randint(1, num_sides)
+        result +=roll
+    result += num_bonus
+    print(result)
