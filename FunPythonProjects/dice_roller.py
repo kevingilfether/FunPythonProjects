464a324a09a923c2ae997b35d9144f838d7d7757
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-04 04:44:24 -0500

    Modularized Code

--- FunPythonProjects/dice_roller.py
+++ FunPythonProjects/dice_roller.py
@@ -11,15 +11,3 @@ def dice_roller(num_dice, num_sides, num_bonus):
         result += roll
     result += num_bonus
     print(result)
-
-#def main():
-#    go_again = True
-#    while go_again:
-#        print("Let's roll some dice! (or type \"quit\" to, well, you know).")
-#        num_dice = int(input("How many dice are you rolling?\n"))
-#        num_sides = int(input("How many sides do the dice have?\n"))
-#        num_bonus = int(input("Any modifiers?\n"))
-#        print("Voila!")
-#        dice_roller(num_dice,num_sides,num_bonus)
-
-guess_a_number
