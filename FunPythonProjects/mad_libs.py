5287207a0fa601efbbe9e315a7262234748eb8f4
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-04 06:03:52 -0500

    Added trailer speak

--- FunPythonProjects/mad_libs.py
+++ FunPythonProjects/mad_libs.py
@@ -7,6 +7,7 @@ plural_noun2 = input("Plural Noun: ")
 verb1 = input("Present Tense Verb: ")
 place1 = input("Place: ")
 noun1 = input("Noun: ")
+adjective3 = input("Adjective: ")
 
 print("""
                 Driving with a trailer
@@ -16,17 +17,17 @@ and are ready to bid "adieu" to the old digs! Congratulations!
 But... wait. Something mysterious {4} in {5}. That's right, you get to drive
 with a {6} in tow. Literally, in tow.
 
-Here are some helpful trailer tips:
+Here are some {7} {6} tips:
 
-1. Be sure to load the trailer evenly, but with a little more weight at the front!
+1. Be sure to load the {6} evenly, but with a little more weight at the front!
 A 60-40 balance is best.
 
 2. Always remember, slow and steady wins the race. Driving a little more slowly helps
 you and your gas consumption. 
 
 3. Pull out a little further into the intersection than you are used to on right turns. 
-Otherwise, your trailer might hop the curb!
+Otherwise, your {6} might hop the curb!
 
 4. Most of all, keep your wits about you and drive with normal safe technique.
-There is no substitute for careful work.
-""".format(adjective1, plural_noun1, adjective2, plural_noun2, verb1, place1, noun1))
+There is no substitute for careful {6} work.
+""".format(adjective1, plural_noun1, adjective2, plural_noun2, verb1, place1, noun1, adjective3))
