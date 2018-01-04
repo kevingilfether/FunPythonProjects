549061ef2914c5ae50a5dcd24039f2aef0122837
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-03 20:35:39 -0500

    Crude copy and function

--- FunPythonProjects/guess_a_number.py
+++ FunPythonProjects/guess_a_number.py
@@ -1,2 +1,11 @@
 #This is a number guessing program
 import random
+
+number_to_guess = random.randint(1,10)
+
+guess = None
+
+while guess != number_to_guess:
+    print("Let's play a guessing game")
+    print("Try to guess a number between 1 and 10!")
+    guess = input()
