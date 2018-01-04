90b9f850c62a075e084ad8450ff8ee04487191ff
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-04 05:13:23 -0500

    Finished mad_libs copy

--- FunPythonProjects/mad_libs.py
+++ FunPythonProjects/mad_libs.py
@@ -1,7 +1,7 @@
 print("We're gonna play some madlibs!")
 
 print("""
-            Driving with a trailer
+                Driving with a trailer
 So, moving day is here. You've packed all the boxes, moved the moist towlettes
 and are ready to bid "adieu" to the old digs! Congratulations!
 
@@ -9,4 +9,16 @@ But... wait. Something mysterious lurks in the background. That's right, you get
 with a trailer in tow. Literally, in tow.
 
 Here are some helpful trailer tips:
+
+1. Be sure to load the trailer evenly, but with a little more weight at the front!
+A 60-40 balance is best.
+
+2. Always remember, slow and steady wins the race. Driving a little more slowly helps
+you and your gas consumption. 
+
+3. Pull out a little further into the intersection than you are used to on right turns. 
+Otherwise, your trailer might hop the curb!
+
+4. Most of all, keep your wits about you and drive with normal safe technique.
+There is no substitute for careful work.
 """)
