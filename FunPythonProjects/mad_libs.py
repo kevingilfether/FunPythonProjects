aed6d7c7228c9f76e3bb756471b473f22961a0ec
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-04 05:08:15 -0500

    Continued madlibs copy

--- FunPythonProjects/mad_libs.py
+++ FunPythonProjects/mad_libs.py
@@ -1,7 +1,12 @@
 print("We're gonna play some madlibs!")
 
 print("""
-test
-test
-test
+            Driving with a trailer
+So, moving day is here. You've packed all the boxes, moved the moist towlettes
+and are ready to bid "adieu" to the old digs! Congratulations!
+
+But... wait. Something mysterious lurks in the background. That's right, you get to drive
+with a trailer in tow. Literally, in tow.
+
+Here are some helpful trailer tips:
 """)
