1b466c78f398af4bb61dd6ec6785518640572132
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-05 04:17:06 -0500

    Made my first room object

--- FunPythonProjects/main.py
+++ FunPythonProjects/main.py
@@ -1,1 +1,5 @@
-import mad_libs
+import room
+
+hall = room.Room("biggish", 
+                 "A richly appointed banquet hall. Incense floats in the air, and a sumptious feast seems like it might occur at any moment.", 
+                 {2: "East and West"})
--- FunPythonProjects/room.py
+++ FunPythonProjects/room.py
@@ -1,5 +1,7 @@
-class room(object):
+class Room(object):
     """This class describes a room in a text-based style game"""
-
-
-
+    
+    def __init__(self, size, description, doors):
+        self.size =size
+        self.description = description
+        self.doors = doors
