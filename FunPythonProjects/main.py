1cf494a6b4f9256534623aad4de7594de671f05e
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-06 13:22:55 -0600

    Added dragon's lair and portal between

--- FunPythonProjects/main.py
+++ FunPythonProjects/main.py
@@ -1,5 +1,17 @@
-import room
+import Room
+import Portal
 
-entrance_hall = room.Room("biggish", 
-                 "A richly appointed banquet hall. Incense floats in the air, and a sumptious feast seems like it might occur at any moment.", 
-                 )
+portal_entrance_hall_to_dragon_lair = Portal.Portal(entrance_hall, dragon_lair)
+entrance_hall = Room.Room("biggish", 
+                 "A richly appointed banquet hall. Incense floats in the air, and a sumptious feast seems like it might occur at any moment. There is a faint glow throw the door to the north",
+                 portal_n = portal_entrance_hall_to_dragon_lair,
+                 portal_e = None,
+                 portal_s = None,
+                 portal_w = None)
+
+dragon_lair = Room.Room("Vast, cavernous even!",
+                        "Oh man, you've really stepped in it now! A giant dragon is sleeping ahead of you on a pile of gold and stuff. It looks asleep and like it didn't need anymore description.",
+                        portal_n = "A dragon is there, you dimwit!",
+                        portal_e = None,
+                        portal_s = portal_entrance_hall_to_dragon_lair,
+                        portal_w = None)
--- FunPythonProjects/room.py
+++ FunPythonProjects/room.py
@@ -2,9 +2,8 @@ class Room(object):
     """This class describes a room in a text-based style game"""
     
     def __init__(self, size, description, portal_n, portal_e, portal_s, portal_w):
-        self.size =size
+        self.size = size
         self.description = description
-        self.doors = doors
         self.portal_n = portal_n
         self.portal_e = portal_e
         self.portal_s = portal_s
