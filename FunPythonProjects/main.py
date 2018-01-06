3aa59b474a8c93b2c394bbd16e64e30bb57867f2
Kevin Gilfether <kevingilfether@gmail.com>
2018-01-06 13:07:12 -0600

    Changed portal and room logic - Both have both now

--- FunPythonProjects/main.py
+++ FunPythonProjects/main.py
@@ -1,5 +1,5 @@
 import room
 
-hall = room.Room("biggish", 
+entrance_hall = room.Room("biggish", 
                  "A richly appointed banquet hall. Incense floats in the air, and a sumptious feast seems like it might occur at any moment.", 
-                 {2: "East and West"})
+                 )
--- FunPythonProjects/room.py
+++ FunPythonProjects/room.py
@@ -1,7 +1,7 @@
 class Room(object):
     """This class describes a room in a text-based style game"""
     
-    def __init__(self, size, description, doors):
+    def __init__(self, size, description, portal_n, portal_e, portal_s, portal_w):
         self.size =size
         self.description = description
         self.doors = doors
